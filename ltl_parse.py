9cbc10939732aba73cbf38735c543e49578332b3
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2018-03-28 19:38:45 +0200

    connectives, main and parse functions

--- ltl_parse.py
+++ ltl_parse.py
@@ -1,15 +1,26 @@
+AND = '&'
+NOT = '~'
+OR = '|'
+IMPLIES = '=>'
+EQUIV = '<=>'
+GLOBALLY = 'G'
+NEXT = 'X'
+UNTIL = 'U'
+
+
 def main():
     with open('ltl_formula.txt','r') as file:
         formula = file.read()
         parse(formula)
 
 def parse(f):
-    print f
+    words = f.split()
 
 
 
 
-    
+
+
 
 
 if __name__ == "__main__":
