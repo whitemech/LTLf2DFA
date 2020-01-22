15830e54e314a4a6e3b50b050eb4ad9be726421c
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2020-01-22 10:52:18 +0100

    introduced ci/cd, tests, init refactoring

---
+++ ltlf2dfa/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for ltlf2dfa."""
+
+from ltlf2dfa.__version__ import __title__, __description__, __url__, __version__
+from ltlf2dfa.__version__ import (
+    __author__,
+    __author_email__,
+    __license__,
+    __copyright__,
+)
---
+++ ltlf2dfa/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Declaration of metadata for the package."""
+
+__title__ = "ltlf2dfa"
+__description__ = "LTLf and PLTLf to Deterministic Finite-state Automata (DFA)"
+__url__ = "https://github.com/francescofuggitti/ltlf2dfa.git"
+__version__ = "0.3.0"
+__author__ = "Francesco Fuggitti"
+__author_email__ = "fuggitti@eecs.yorku.ca"
+__license__ = "MIT License"
+__copyright__ = "2019 Francesco Fuggitti"
