39900ddb09767f50dba843dd332f764fcc187e46
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2020-01-22 11:06:48 +0100

    add docs and tests folders, makefile, Pipfile, setup.cfg and tox.ini

---
+++ AUTHORS.md
@@ -0,0 +1,9 @@
+# Credits
+
+## Maintainer
+
+- [Francesco Fuggitti](https://github.com/francescofuggitti) <[fuggitti@diag.uniroma1.it](mailto:fuggitti@diag.uniroma1.it)>
+
+## Contributors
+
+None yet. [Why not be the first](./contributing.md)?
---
+++ tests/test_parsing.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Test the parsing module."""
+# import os
+# import tempfile
+# from collections import OrderedDict
+# from pathlib import Path
+#
+# import pytest
+#
+# from ltlf2dfa.Lexer import MyLexer
+# from ltlf2dfa.Parser import MyParser
+#
+# from ltlf2dfa.core import HOA, Acceptance, Atom, AtomType, And, TrueAcceptance, AliasLabelExpression, \
+#     AtomLabelExpression, AndLabelExpression, NotLabelExpression, State, Edge, TrueLabelExpression, HOAHeader, HOABody
+# from .conftest import TEST_ROOT_DIR
