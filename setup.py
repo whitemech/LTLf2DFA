aedd85bbf2ae2798c482ecd9f32e74dfccfb8692
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2020-08-25 23:06:29 +0200

    update setup and dependencies

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open(os.path.join(here, 'ltlf2dfa', '__version__.py'), 'r') as f:
 
 
 install_requires = [
-    "lark-parser",
+    "lark-parser>=0.9.0,<1",
     "sympy"
 ]
 
@@ -50,7 +50,7 @@ setup(
     long_description=readme + '\n\n' + history,
     long_description_content_type="text/markdown",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 2 - Alpha',
         'Intended Audience :: Education',
         'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Natural Language :: English',
